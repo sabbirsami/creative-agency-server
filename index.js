2ea19c376a8aa2aef8a1451c7562313d995d55bb
SabbirSami <smd71430@gmail.com>
2022-06-29 23:54:03 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.up3hj.mongodb.net/?retryWrites=true&w=majority`;
 
 app.use(cors());
 app.use(express.json());
 
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
 async function run() {
     try {
+        await client.connect();
+        const userCollection = client.db("creativeAgency").collection("users");
+        console.log("Here");
     } finally {
     }
 }
